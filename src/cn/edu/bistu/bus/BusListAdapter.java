b64f5e4e03ed28d93118ccaef56fc233beb95018
hcjcch <huangchen@mail.bistu.edu.cn>
2014-03-16 21:55:49 +0800

    bus show

---
+++ src/cn/edu/bistu/bus/BusListAdapter.java
@@ -0,0 +1,124 @@
+package cn.edu.bistu.bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.icampus2_2.R;
+
+import cn.edu.bistu.busData.Bus;
+import cn.edu.bistu.busData.CatBus;
+import cn.edu.bistu.busData.EveryBus;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class BusListAdapter extends BaseAdapter {
+	private Context context;
+	private CatBus tongQingbus;
+	private CatBus jiaoXueBus;
+	private ArrayList<Object> bus;
+	private static final int TITLE_ITEM = 0;
+	private static final int BUS_LINE = 1;
+
+	public BusListAdapter(Context context, List<Bus> list) {
+		super();
+		this.context = context;
+		tongQingbus = list.get(0).getCatbus();
+		jiaoXueBus = list.get(1).getCatbus();
+		bus = new ArrayList<Object>();
+		bus.add("通勤班车");
+		for (EveryBus everyBus : tongQingbus.getCatbus()) {
+			bus.add(everyBus);
+		}
+		bus.add("教学班车");
+		for (EveryBus everyBus : jiaoXueBus.getCatbus()) {
+			bus.add(everyBus);
+		}
+	}
+
+	public BusListAdapter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		// TODO Auto-generated method stub
+		if (bus.get(position) instanceof EveryBus) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public boolean isEnabled(int position) {
+		// TODO Auto-generated method stub
+		return bus.get(position) instanceof EveryBus;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return bus.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return bus.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		if (getItemViewType(arg0) == TITLE_ITEM) {
+			arg1 = LayoutInflater.from(context).inflate(
+					R.layout.bus_special_item, null);
+			TextView textView = (TextView) arg1.findViewById(R.id.textView1);
+			textView.setText((String) bus.get(arg0));
+		} else {
+			BusItem busItem = null;
+			if (arg1 == null) {
+				busItem = new BusItem();
+				arg1 = LayoutInflater.from(context)
+						.inflate(R.layout.bus_item, null);
+				busItem.line1 = (TextView)arg1.findViewById(R.id.line1);
+				busItem.line2 = (TextView)arg1.findViewById(R.id.line2);
+				arg1.setTag(busItem);
+			}else {
+				busItem = (BusItem)arg1.getTag();
+			}
+			EveryBus everyBus = (EveryBus)bus.get(arg0);
+			busItem.line1.setText(everyBus.getBusName());
+			busItem.line2.setText(everyBus.getBusName());
+		}
+		return arg1;
+	}
+
+	class SpecialItem{
+		TextView textView;
+	}
+	class BusItem{
+		TextView line1;
+		TextView line2;
+		TextView goTimeTextView;
+		TextView backTime;
+	}
+}
--- src/cn/edu/bistu/bus/BusShow.java
+++ src/cn/edu/bistu/bus/BusShow.java
@@ -1,17 +1,31 @@
 package cn.edu.bistu.bus;
 
+import java.util.List;
+
+import org.apache.http.Header;
+
+import cn.edu.bistu.busData.Bus;
+import cn.edu.bistu.busData.JsonBus;
+
+import com.example.icampus2_2.ICampus;
 import com.example.icampus2_2.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class BusShow extends Activity {
-	private ListView bus;
+	private ListView busList;
+	private List<Bus> bus;
+
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@SuppressLint("NewApi")
 	@Override
@@ -22,11 +36,61 @@ public class BusShow extends Activity {
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		actionBar.show();
+		init();
+		show();
 	}
-	public void init(){
-		bus = (ListView)findViewById(R.id.busList);
+
+	public void init() {
+		busList = (ListView) findViewById(R.id.busList);
 	}
-	public void show(){
-		
+
+	public void show() {
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("http://m.bistu.edu.cn/newapi/bus.php",
+				new AsyncHttpResponseHandler() {
+
+					@Override
+					public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+							Throwable arg3) {
+						// TODO Auto-generated method stub
+						super.onFailure(arg0, arg1, arg2, arg3);
+					}
+
+					@Override
+					public void onStart() {
+						// TODO Auto-generated method stub
+						super.onStart();
+					}
+
+					@Override
+					public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+						// TODO Auto-generated method stub
+						super.onSuccess(arg0, arg1, arg2);
+						String string = new String(arg2);
+						bus = (new JsonBus()).getList(string);
+						// System.out.println(bus);
+						BusListAdapter adapter = new BusListAdapter(
+								BusShow.this, bus);
+						busList.setAdapter(adapter);
+					}
+
+				});
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, ICampus.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
