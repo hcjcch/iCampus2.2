4ca7c092694a57254b2ed0d286a5042a67889294
hcjcch <huangchen@mail.bistu.edu.cn>
2014-03-16 18:01:17 +0800

    bus show

--- src/cn/edu/bistu/bus/BusShow.java
+++ src/cn/edu/bistu/bus/BusShow.java
@@ -2,14 +2,31 @@ package cn.edu.bistu.bus;
 
 import com.example.icampus2_2.R;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class BusShow extends Activity {
+	private ListView bus;
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.bus);
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.show();
+	}
+	public void init(){
+		bus = (ListView)findViewById(R.id.busList);
+	}
+	public void show(){
+		
 	}
 }
